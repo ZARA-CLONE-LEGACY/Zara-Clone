945b7fda63d64f9747273ece7191e4020d3e1c5e
Nacef19 <117386346+Nacef19@users.noreply.github.com>
2023-05-23 13:10:18 +0100

    first step into refactoring the authentication

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <Login />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Login from './components/Authentication/Login.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Login,
+  },
+});
+</script>
 
 <style>
 #app {
