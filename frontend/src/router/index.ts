b9e3b7a920a698c61f46aeedd644bcf92abb2306
imen <mekni7058@gmail.com>
2023-05-23 19:18:04 -0700

    search

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/Login.vue' 
 import HomeView from '../components/Home.vue'
 import Signup from '../components/Signup.vue'
+import Search from '../components/Search.vue'
 const routes = [
   { path: '/', component : HomeView }, 
   { path: '/login', component: Login }, 
   { path: '/signup', component: Signup }, 
+  { path: '/search', component: Search },
 ];
 
 const router = createRouter({
